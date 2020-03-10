ad622136cfb25b822031ead0fb7668b356c870b4
Leander Jedamus <ljedamus@web.de>
2020-03-10 11:32:50 +0100

    added vector to matrizen.py

--- matrizen.py
+++ matrizen.py
@@ -2,14 +2,16 @@
 # coding=utf-8 -*- python -*-
 
 # erzeugt Dienstag, 10. März 2020 10:55 (C) 2020 von Leander Jedamus
-# modifiziert Dienstag, 10. März 2020 11:19 von Leander Jedamus
+# modifiziert Dienstag, 10. März 2020 11:32 von Leander Jedamus
 
 from __future__ import print_function
 
 n = 3
 bits = [];
+vector = [];
 for i in range(2**n):
   bits.append("")
+  vector.append("")
 for i in range(2**n):
   print(i+1)
   for j in range(n):
@@ -20,6 +22,15 @@ for i in range(2**n):
       #print("0",end="")
       bits[i-1] += "0"
   print(bits[i-1])
+  for j in range(2**n):
+    if (j == i):
+      #print("1",end="")
+      vector[i-1] += "1"
+    else:
+      #print("0",end="")
+      vector[i-1] += "0"
+  print(vector[i-1])
+
 
 # vim:ai sw=2 sts=4 expandtab
 
