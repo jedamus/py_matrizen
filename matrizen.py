06f0a0f70b845444d9db71ac2e2224b13269e63c
Leander Jedamus <ljedamus@web.de>
2020-03-10 11:21:17 +0100

    added bits-list to matrizen.py

--- matrizen.py
+++ matrizen.py
@@ -2,19 +2,24 @@
 # coding=utf-8 -*- python -*-
 
 # erzeugt Dienstag, 10. März 2020 10:55 (C) 2020 von Leander Jedamus
-# modifiziert Dienstag, 10. März 2020 11:10 von Leander Jedamus
+# modifiziert Dienstag, 10. März 2020 11:19 von Leander Jedamus
 
 from __future__ import print_function
 
 n = 3
+bits = [];
+for i in range(2**n):
+  bits.append("")
 for i in range(2**n):
   print(i+1)
   for j in range(n):
     if (i & 2**(n-j-1) != 0):
-      print("1",end="")
+      #print("1",end="")
+      bits[i-1] += "1"
     else:
-      print("0",end="")
-  print()
+      #print("0",end="")
+      bits[i-1] += "0"
+  print(bits[i-1])
 
 # vim:ai sw=2 sts=4 expandtab
 
