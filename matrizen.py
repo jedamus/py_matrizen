26bcc22b9d3dbe206d35395015a0bd9d7904dc8e
Leander Jedamus <ljedamus@web.de>
2020-03-10 11:12:12 +0100

    added bit-operations to matrizen.py

--- matrizen.py
+++ matrizen.py
@@ -2,13 +2,19 @@
 # coding=utf-8 -*- python -*-
 
 # erzeugt Dienstag, 10. März 2020 10:55 (C) 2020 von Leander Jedamus
-# modifiziert Dienstag, 10. März 2020 11:01 von Leander Jedamus
+# modifiziert Dienstag, 10. März 2020 11:10 von Leander Jedamus
 
 from __future__ import print_function
 
 n = 3
 for i in range(2**n):
   print(i+1)
+  for j in range(n):
+    if (i & 2**(n-j-1) != 0):
+      print("1",end="")
+    else:
+      print("0",end="")
+  print()
 
 # vim:ai sw=2 sts=4 expandtab
 
