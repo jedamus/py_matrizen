8e62876786e29a1a0067cbec9330a4bd38ef2901
Leander Jedamus <ljedamus@web.de>
2020-03-10 11:01:29 +0100

    first commit for matrizen.py

---
+++ matrizen.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8 -*- python -*-
+
+# erzeugt Dienstag, 10. März 2020 10:55 (C) 2020 von Leander Jedamus
+# modifiziert Dienstag, 10. März 2020 11:01 von Leander Jedamus
+
+from __future__ import print_function
+
+n = 3
+for i in range(2**n):
+  print(i+1)
+
+# vim:ai sw=2 sts=4 expandtab
+
