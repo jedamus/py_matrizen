cf124204b0bb07245ea4afb1acb709c6584ee3ad
Leander Jedamus <ljedamus@web.de>
2020-03-11 10:34:49 +0100

    added vector and logging

--- matrizen.py
+++ matrizen.py
@@ -2,35 +2,66 @@
 # coding=utf-8 -*- python -*-
 
 # erzeugt Dienstag, 10. März 2020 10:55 (C) 2020 von Leander Jedamus
+# modifiziert Mittwoch, 11. März 2020 10:33 von Leander Jedamus
 # modifiziert Dienstag, 10. März 2020 11:32 von Leander Jedamus
 
 from __future__ import print_function
 
+import sys
+if int(sys.version_info.major) < 3:
+  my_input = raw_input
+else:
+  my_input = input
+
+
+import os
+import sys
+import logging
+
+log_path_and_filename = os.path.join("/tmp","matrizen.log")
+file_handler = logging.FileHandler(log_path_and_filename)
+stdout_handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s",
+                              "%d.%m.%Y %H:%M:%S")
+file_handler.setFormatter(formatter)
+stdout_handler.setFormatter(formatter)
+log = logging.getLogger()
+log.addHandler(file_handler)
+log.addHandler(stdout_handler)
+log.setLevel(logging.DEBUG)
+
 n = 3
+power = 2**n
 bits = [];
+vec_s = [];
 vector = [];
-for i in range(2**n):
+for i in range(power):
   bits.append("")
-  vector.append("")
-for i in range(2**n):
-  print(i+1)
+  vec_s.append("")
+  vector.append([]);
+
+for i in range(power):
+  for j in range(power):
+    vector[j-1].append(0)
+
+log.debug("vector = {vector:s}".format(vector=str(vector)))
+
+for i in range(power):
+  log.debug("i = {i:s}".format(i=str(i+1)))
   for j in range(n):
     if (i & 2**(n-j-1) != 0):
-      #print("1",end="")
       bits[i-1] += "1"
     else:
-      #print("0",end="")
       bits[i-1] += "0"
-  print(bits[i-1])
-  for j in range(2**n):
+  log.debug("bits[{i:d}] = {bits:s}".format(i=i,bits=bits[i-1]))
+  for j in range(power):
     if (j == i):
-      #print("1",end="")
-      vector[i-1] += "1"
+      vec_s[i-1] += "1"
+      vector[i-1][j-1] = 1
     else:
-      #print("0",end="")
-      vector[i-1] += "0"
-  print(vector[i-1])
-
+      vec_s[i-1] += "0"
+  log.debug("vec_s[{i:d}] = {vec_s:s}".format(i=i,vec_s=vec_s[i-1]))
+log.debug("vector = {vector:s}".format(vector=str(vector)))
 
 # vim:ai sw=2 sts=4 expandtab
 
