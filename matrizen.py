f49a9e973a8cc364316233f082a32cb87bd32aca
Leander Jedamus <ljedamus@web.de>
2020-03-11 10:56:56 +0100

    added mat_mul

--- matrizen.py
+++ matrizen.py
@@ -2,7 +2,7 @@
 # coding=utf-8 -*- python -*-
 
 # erzeugt Dienstag, 10. März 2020 10:55 (C) 2020 von Leander Jedamus
-# modifiziert Mittwoch, 11. März 2020 10:33 von Leander Jedamus
+# modifiziert Mittwoch, 11. März 2020 10:55 von Leander Jedamus
 # modifiziert Dienstag, 10. März 2020 11:32 von Leander Jedamus
 
 from __future__ import print_function
@@ -12,7 +12,6 @@ if int(sys.version_info.major) < 3:
   my_input = raw_input
 else:
   my_input = input
-
 
 import os
 import sys
@@ -30,38 +29,60 @@ log.addHandler(file_handler)
 log.addHandler(stdout_handler)
 log.setLevel(logging.DEBUG)
 
-n = 3
-power = 2**n
-bits = [];
-vec_s = [];
-vector = [];
-for i in range(power):
-  bits.append("")
-  vec_s.append("")
-  vector.append([]);
+def mat_mul(s_vector,z_vector,n):
+  power = 2**n
+  mat = [];
+  for i in range(power):
+    mat.append([])
 
-for i in range(power):
-  for j in range(power):
-    vector[j-1].append(0)
+  for i in range(power):
+    for j in range(power):
+      mat[i-1].append(0)
 
-log.debug("vector = {vector:s}".format(vector=str(vector)))
+  for i in range(power):
+    for j in range(power):
+      mat[i-1][j-1]= s_vector[i-1]*z_vector[j-1]
+  
+  return(mat)
 
-for i in range(power):
-  log.debug("i = {i:s}".format(i=str(i+1)))
-  for j in range(n):
-    if (i & 2**(n-j-1) != 0):
-      bits[i-1] += "1"
-    else:
-      bits[i-1] += "0"
-  log.debug("bits[{i:d}] = {bits:s}".format(i=i,bits=bits[i-1]))
-  for j in range(power):
-    if (j == i):
-      vec_s[i-1] += "1"
-      vector[i-1][j-1] = 1
-    else:
-      vec_s[i-1] += "0"
-  log.debug("vec_s[{i:d}] = {vec_s:s}".format(i=i,vec_s=vec_s[i-1]))
-log.debug("vector = {vector:s}".format(vector=str(vector)))
+inp = my_input('n = ')
+if inp.isdigit():
+  n = int(inp)
+  power = 2**n
+  bits = [];
+  vec_s = [];
+  vector = [];
+  mat = [];
+  for i in range(power):
+    bits.append("")
+    vec_s.append("")
+    vector.append([]);
+
+  for i in range(power):
+    for j in range(power):
+      vector[j-1].append(0)
+
+  log.debug("vector = {vector:s}".format(vector=str(vector)))
+
+  for i in range(power):
+    log.debug("i = {i:s}".format(i=str(i+1)))
+    for j in range(n):
+      if (i & 2**(n-j-1) != 0):
+        bits[i-1] += "1"
+      else:
+        bits[i-1] += "0"
+    log.debug("bits[{i:d}] = {bits:s}".format(i=i,bits=bits[i-1]))
+    for j in range(power):
+      if (j == i):
+        vec_s[i-1] += "1"
+        vector[i-1][j-1] = 1
+      else:
+        vec_s[i-1] += "0"
+    log.debug("vec_s[{i:d}] = {vec_s:s}".format(i=i,vec_s=vec_s[i-1]))
+  log.debug("vector = {vector:s}".format(vector=str(vector)))
+  mat.append(mat_mul(vector[3],vector[2],n))
+  log.debug("mat[0] = {mat:s}".format(mat=str(mat[0])))
+
 
 # vim:ai sw=2 sts=4 expandtab
 
