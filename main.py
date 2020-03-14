f623e4106d585015bcd69e1064db78a2f60c6f70
Leander Jedamus <ljedamus@web.de>
2020-03-14 12:27:21 +0100

    added main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8 -*- python -*-
+
+# erzeugt Samstag, 14. März 2020 12:26 (C) 2020 von Leander Jedamus
+# modifiziert Samstag, 14. März 2020 12:26 von Leander Jedamus
+
+from __future__ import print_function
+
+# vim:ai sw=2 sts=4 expandtab
+
