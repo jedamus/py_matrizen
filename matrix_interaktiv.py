9c1990c4a973b608bc33792b1233a4e840ef1e36
Leander Jedamus <ljedamus@web.de>
2020-03-14 08:46:45 +0100

    moved parts to matrizen

--- matrix_interaktiv.py
+++ matrix_interaktiv.py
@@ -2,10 +2,12 @@
 # coding=utf-8 -*- python -*-
 
 # erzeugt Dienstag, 10. März 2020 10:55 (C) 2020 von Leander Jedamus
+# modifiziert Samstag, 14. März 2020 08:46 von Leander Jedamus
 # modifiziert Mittwoch, 11. März 2020 17:35 von Leander Jedamus
 # modifiziert Dienstag, 10. März 2020 11:32 von Leander Jedamus
 
 from __future__ import print_function
+import matrizen
 
 import sys
 if int(sys.version_info.major) < 3:
@@ -30,64 +32,18 @@ log.addHandler(stdout_handler)
 # log.setLevel(logging.DEBUG)
 log.setLevel(logging.INFO)
 
-def mat_mul(s_vector,z_vector,n):
-  power = 2**n
-  mat = [];
-  for i in range(power):
-    mat.append([])
-
-  for i in range(power):
-    for j in range(power):
-      mat[i-1].append(0)
-
-  for i in range(power):
-    for j in range(power):
-      mat[i-1][j-1]= s_vector[i-1]*z_vector[j-1]
-  
-  return(mat)
-
 inp = my_input('n = ')
 if inp.isdigit():
   n = int(inp)
+  (bits, vector) = matrizen.bits_and_vector(n)
   power = 2**n
-  bits = []
-  vec_s = []
-  vector = []
   matrix = []
   for i in range(power):
-    bits.append("")
-    vec_s.append("")
-    vector.append([])
     matrix.append([])
 
   for i in range(power):
     for j in range(power):
-      vector[j].append(0)
       matrix[j].append(0)
-
-  if log.isEnabledFor(logging.DEBUG):
-    log.debug("vector = {vector:s}".format(vector=str(vector)))
-
-  for i in range(power):
-    if log.isEnabledFor(logging.DEBUG):
-      log.debug("i = {i:s}".format(i=str(i+1)))
-    for j in range(n):
-      if (i & 2**(n-j-1) != 0):
-        bits[i] += "1"
-      else:
-        bits[i] += "0"
-    if log.isEnabledFor(logging.DEBUG):
-      log.debug("bits[{i:d}] = {bits:s}".format(i=i,bits=bits[i]))
-    for j in range(power):
-      if (j == i):
-        vec_s[i] += "1"
-        vector[i][j] = 1
-      else:
-        vec_s[i] += "0"
-    if log.isEnabledFor(logging.DEBUG):
-      log.debug("vec_s[{i:d}] = {vec_s:s}".format(i=i,vec_s=vec_s[i]))
-  if log.isEnabledFor(logging.DEBUG):
-    log.debug("vector = {vector:s}".format(vector=str(vector)))
 
   # bra und ket einlesen
   bra = []
@@ -131,7 +87,7 @@ if inp.isdigit():
         k = int(k)
         ket[k-1] = ""
         z_vector = vector[ket_ind[k-1]]
-        mat = mat_mul(s_vector,z_vector,n)
+        mat = matrizen.mat_mul(s_vector,z_vector,n)
         if log.isEnabledFor(logging.DEBUG):
           log.debug("bits = {bits:s}".format(bits=str(bits)))
           log.debug("bits[{index:d}] = {bits:s}".format(index=ket_ind[k-1],bits=bits[ket_ind[k-1]]))
