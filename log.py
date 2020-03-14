76f748fb5b73f43b4d64b5f315630a2c1c79a886
Leander Jedamus <ljedamus@web.de>
2020-03-14 12:27:55 +0100

    added filter for __main__

--- log.py
+++ log.py
@@ -2,6 +2,7 @@
 #qpy:3
 #qpy:console
 # erzeugt Montag, 13. Mai 2019 07:05 (C) 2019 von Leander Jedamus
+# modifiziert Samstag, 14. März 2020 12:27 von Leander Jedamus
 # modifiziert Montag, 13. Mai 2019 09:37 von Leander Jedamus
 
 from __future__ import print_function
@@ -9,7 +10,7 @@ import logging
 
 def addFilter(logger):
   logger.addFilter(logging.Filter("dialogs"))
-#  logger.addFilter(logging.Filter("__main__"))
+  logger.addFilter(logging.Filter("__main__"))
 
 # vim:ai sw=2 sts=4 expandtab
 
