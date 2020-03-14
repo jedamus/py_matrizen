25a63fefd692115f7712a470d7d46a7b2a81b02c
Leander Jedamus <ljedamus@web.de>
2020-03-14 08:37:39 +0100

    added bits_and_vector

--- matrix_aus_datei.py
+++ matrix_aus_datei.py
@@ -2,26 +2,28 @@
 # coding=utf-8 -*- python -*-
 
 # erzeugt Samstag, 14. März 2020 07:37 (C) 2020 von Leander Jedamus
-# modifiziert Samstag, 14. März 2020 08:17 von Leander Jedamus
+# modifiziert Samstag, 14. März 2020 08:33 von Leander Jedamus
 
 from __future__ import print_function
 import re
+import matrizen
 import os
 import sys
 import logging
 
-log_path_and_filename = os.path.join("/tmp","matrizen.log")
-file_handler = logging.FileHandler(log_path_and_filename)
+
+logger_path_and_filename = os.path.join("/tmp","matrizen.logger")
+file_handler = logging.FileHandler(logger_path_and_filename)
 stdout_handler = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s",
                               "%d.%m.%Y %H:%M:%S")
 file_handler.setFormatter(formatter)
 stdout_handler.setFormatter(formatter)
-log = logging.getLogger()
-log.addHandler(file_handler)
-log.addHandler(stdout_handler)
-log.setLevel(logging.DEBUG)
-# log.setLevel(logging.INFO)
+logger = logging.getLogger()
+logger.addHandler(file_handler)
+logger.addHandler(stdout_handler)
+logger.setLevel(logging.DEBUG)
+# logger.setLevel(logging.INFO)
 
 
 datei = open("matrix_xor.dat","r")
@@ -40,21 +42,27 @@ for line in datei:
   if (re.match(reg_empty,line)):
     continue
 
-  if log.isEnabledFor(logging.DEBUG):
-    log.debug("line = " + line)
+  if logger.isEnabledFor(logging.DEBUG):
+    logger.debug("line = " + line)
 
   if not has_n:
     # n auslesen
     if (re.match(reg_n,line)):
-      if log.isEnabledFor(logging.DEBUG):
-        log.debug("n gefunden")
+      if logger.isEnabledFor(logging.DEBUG):
+        logger.debug("n gefunden")
       n = re.sub(reg_n,"\g<1>",line)
       if n.isdigit():
         n = int(n)
         has_n = True
-        log.debug("n = " + str(n))
+        logger.debug("n = " + str(n))
+        (bits,vector) = matrizen.bits_and_vector(n)
+        logger.debug("bits = " + str(bits))
+        logger.debug("vector = " + str(vector))
       else:
-        log.fatal("n is not a decimal")
+        logger.fatal("n is not a decimal")
+  else:
+    pass
+
 datei.close()
 
 # vim:ai sw=2 sts=4 expandtab
