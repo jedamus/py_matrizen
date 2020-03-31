1899a7d17e4217bb1e5cf654bd13443be776fbcb
Leander Jedamus <ljedamus@web.de>
2020-03-31 20:55:14 +0200

    added matrix_create

--- translate.sh
+++ translate.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
 # erzeugt Samstag, 14. März 2020 13:51 (C) 2020 von Leander Jedamus
+# modifiziert Dienstag, 31. März 2020 20:52 von Leander Jedamus
 # modifiziert Samstag, 14. März 2020 13:52 von Leander Jedamus
 
 translate.pl --project=matrizen --version=1.1 --file=main.py \
-             --file=matrix_aus_datei.py --file=matrix_interaktiv.py
+             --file=matrix_aus_datei.py --file=matrix_interaktiv.py \
+	     --file=matrix_create.py
 
 # vim:ai sw=2
 
