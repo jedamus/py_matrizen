8ce2996c5170a9ad21db76ec7b3aaa03716c056b
Leander Jedamus <ljedamus@web.de>
2020-03-31 22:26:00 +0200

    added binaer.py

---
+++ binaer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8 -*- python -*-
+
+# erzeugt Dienstag, 31. März 2020 22:18 (C) 2020 von Leander Jedamus
+# modifiziert Dienstag, 31. März 2020 22:23 von Leander Jedamus
+
+from __future__ import print_function
+
+lbits = "01011100"
+print("lbits = {lbits:s}".format(lbits=lbits))
+n = int(lbits,2)
+print("n = {n:d}".format(n=n))
+
+# vim:ai sw=2 sts=4 expandtab
+
